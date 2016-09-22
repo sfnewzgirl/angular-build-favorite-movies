56bdf354d23c2c0f2b8478c0e021dd7c0d143772
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-22 09:45:18 -0700

    fixes api displays in html

--- public/scripts/controllers/MoviesIndexController.js
+++ public/scripts/controllers/MoviesIndexController.js
@@ -5,8 +5,9 @@ MoviesIndexController.$inject=['MovieService'];
 function MoviesIndexController(MovieService) {
   console.log('index controller called');
   var vm = this;
-  vm.movies = [{title: 'The Godfather'}, {title: 'Legends of the Fall'}, {title: 'Wedding Crashers'}, {title: 'August: Osage County'}, {title: 'The Hunger Games'}, {title: 'Clue'}];
-  angular.forEach(vm.movies, function(key, value) {
+  vm.movies= [];
+  var movieTitles = [{title: 'The Godfather'}, {title: 'Legends of the Fall'}, {title: 'Wedding Crashers'}, {title: 'August: Osage County'}, {title: 'The Hunger Games'}, {title: 'Clue'}];
+  angular.forEach(movieTitles, function(key, value) {
     console.log('each movie', key, value);
     var movieId = key.title;
     getMovie(movieId);
@@ -15,7 +16,7 @@ function MoviesIndexController(MovieService) {
       console.log('get movie called', movieId);
       MovieService.get(movieId).then(function(data){
         console.log('here is the movie data ', data);
-        vm.movie = data;
+        vm.movies.push(data);
       });
     }
   });
