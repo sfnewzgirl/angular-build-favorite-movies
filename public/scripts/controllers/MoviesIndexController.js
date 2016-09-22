bd2975824703b53edbfc9d249f32471b19fae818
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-21 21:06:05 -0700

    vote clicks are not movie specific

--- public/scripts/controllers/MoviesIndexController.js
+++ public/scripts/controllers/MoviesIndexController.js
@@ -5,8 +5,8 @@ MoviesIndexController.$inject=['MovieService'];
 function MoviesIndexController(MovieService) {
   console.log('index controller called');
   var vm = this;
-  var movieTitles = [{title: 'The Godfather'}, {title: 'Legends of the Fall'}, {title: 'Wedding Crashers'}, {title: 'August: Osage County'}, {title: 'The Hunger Games'}, {title: 'Clue'}];
-  angular.forEach(movieTitles, function(key, value) {
+  vm.movies = [{title: 'The Godfather'}, {title: 'Legends of the Fall'}, {title: 'Wedding Crashers'}, {title: 'August: Osage County'}, {title: 'The Hunger Games'}, {title: 'Clue'}];
+  angular.forEach(vm.movies, function(key, value) {
     console.log('each movie', key, value);
     var movieId = key.title;
     getMovie(movieId);
@@ -17,16 +17,15 @@ function MoviesIndexController(MovieService) {
         console.log('here is the movie data ', data);
         vm.movie = data;
       });
-      return vm.movie;
     }
   });
 
-  vm.voteUp = function() {
-    console.log('vote up button clicked');
+  vm.voteUp = function(movieId) {
+    console.log('vote up button clicked', movieId);
   }
 
-  vm.voteDown = function() {
-    console.log('vote down button clicked');
+  vm.voteDown = function(movieId) {
+    console.log('vote down button clicked', movieId);
   }
 
 console.log('index controller exit, query get');
