3eed6142be8a3a8ee3e31f22b7b8b7ca4ce3a110
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-21 19:21:07 -0700

    renames array

--- public/scripts/controllers/MoviesIndexController.js
+++ public/scripts/controllers/MoviesIndexController.js
@@ -5,8 +5,8 @@ MoviesIndexController.$inject=['MovieService'];
 function MoviesIndexController(MovieService) {
   console.log('index controller called');
   var vm = this;
-  vm.movies = [{title: 'The Godfather'}, {title: 'Legends of the Fall'}, {title: 'Wedding Crashers'}, {title: 'August: Osage County'}, {title: 'The Hunger Games'}, {title: 'Clue'}];
-  angular.forEach(vm.movies, function(key, value) {
+  var movieTitles = [{title: 'The Godfather'}, {title: 'Legends of the Fall'}, {title: 'Wedding Crashers'}, {title: 'August: Osage County'}, {title: 'The Hunger Games'}, {title: 'Clue'}];
+  angular.forEach(movieTitles, function(key, value) {
     console.log('each movie', key, value);
     var movieId = key.title;
     getMovie(movieId);
