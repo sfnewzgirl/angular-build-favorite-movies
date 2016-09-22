c8d698477f14d0ecc62e1f3e8a45a8e4a2cc3755
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-21 17:08:09 -0700

    changes movies in array

--- public/scripts/controllers/MoviesIndexController.js
+++ public/scripts/controllers/MoviesIndexController.js
@@ -5,7 +5,7 @@ MoviesIndexController.$inject=['MovieService'];
 function MoviesIndexController(MovieService) {
   console.log('index controller called');
   var vm = this;
-  vm.movies = [{title: 'Clue'}, {title: 'Frozen'}, {title: 'The Hunger Games'}];
+  vm.movies = [{title: 'The Godfather'}, {title: 'Legends of the Fall'}, {title: 'Wedding Crashers'}, {title: 'August: Osage County'}, {title: 'The Hunger Games'}, {title: 'Clue'}];
   angular.forEach(vm.movies, function(key, value) {
     console.log('each movie', key, value);
     var movieId = key.title;
